c3e8c4ca573b0a221a0fb82fe9de4651b5432fbc
Yizhu Wang <abamboo319@gmail.com>
2015-02-07 09:08:47 -0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
 });
 
 
+function getData() {
+	$.getJSON("js/baseballYizhuWang", function(data){
+    playerData = data;
+    drawChart();
+
+	});
+}
 
 
+function drawChart () {
 
+     $(".player-name").html(playerData.name);                              /* loop */
+    $.each(playerData.stats, function(i, item){
+        var width = item.HR * 50;
+             $(".chart").append(
+                "<div class='col-md-12 bar-container'>"+
+                "<div class='year'>"+item.year+"</div>"+
+                "<div class='bar' style='width:"+width+"px'></div>"+
+                "<div class='total'>"+item.HR+"</div>"+
+                "</div>");
+    });
 
+}
